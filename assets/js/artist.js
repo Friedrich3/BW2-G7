00abe8ef7019c6f9d656eb85b73b129c3b7aa13f
Federico Tonti <148573975+Friedrich3@users.noreply.github.com>
2024-12-17 11:24:48 +0100

    Artist Layout + fix format

---
+++ assets/js/artist.js
@@ -0,0 +1,7 @@
+
+
+//Funzione per aggiungere o togliere la classe expanded alla sidebar
+function toggleMenu() {
+    const sidebar = document.getElementById('sidebar');
+    sidebar.classList.toggle('expanded'); // Aggiunge o rimuove la classe "expanded"
+}
