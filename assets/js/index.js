3c4fcb42e51a6dc790b89ae5b92bfcb45e2473b4
Federico Tonti <148573975+Friedrich3@users.noreply.github.com>
2024-12-17 09:10:42 +0100

    rimosso il source SKD di spotify

--- assets/js/index.js
+++ assets/js/index.js
@@ -57,16 +57,6 @@ async function getData(token) {
 
 
 
-window.onSpotifyWebPlaybackSDKReady = () => {
-    const token = '[My access token]';
-    const player = new Spotify.Player({
-      name: 'Web Playback SDK Quick Start Player',
-      getOAuthToken: cb => { cb(token); },
-      volume: 0.5
-    });
-}
-
-
 
 
 
