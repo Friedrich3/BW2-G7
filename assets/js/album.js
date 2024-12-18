1d524554594b30cd5e6bed0326987a6efbf4849d
Federico Tonti <148573975+Friedrich3@users.noreply.github.com>
2024-12-18 13:03:03 +0100

    stampa hero section

--- assets/js/album.js
+++ assets/js/album.js
@@ -3,6 +3,8 @@ const param = new URLSearchParams(window.location.search).get("id");
 
 const albumUrl = `https://striveschool-api.herokuapp.com/api/deezer/album/${param}`;
 
+let arraySong = [];
+let album = {};
 
 
 
@@ -11,10 +13,10 @@ window.addEventListener("load", init());
 function init() {
   getAlbum();
 
+
 };
 
 async function getAlbum() {
-  console.log(albumUrl)
   try {
     let response = await fetch(albumUrl, {
       method: "GET",
@@ -23,10 +25,57 @@ async function getAlbum() {
       },
     });
     let data = await response.json();
-    console.log(data);
+    album = { ...data };
+    arraySong = data.tracks.data;
+    console.log(album);
+    printHero(album);
+    printSong(arraySong);
+
   } catch (error) {
-    console.log("Error: " + error)
+    console.log("Error: " + error);
   }
+};
+
+
+
+function printHero(album) {
+  let imgAlbum = document.getElementById("imgAlbum");
+  let imgTitle = document.getElementById("title");
+  let bandImage = document.getElementById("imgBand");
+  let imgBandLink = document.getElementById("imgBandLink");
+  let bandName = document.getElementById("bandName");
+  let bandNameLink = document.getElementById("bandNameLink");
+  let albumInfo = document.getElementById("albumInfo");
+
+  imgAlbum.setAttribute("src", album.cover_medium);
+  imgTitle.innerText =  `${album.title}`;
+
+  imgBandLink.setAttribute("href",`artist.html?id=${album.artist.id}`);
+  bandImage.setAttribute("src", album.artist.picture_medium);
+
+  bandNameLink.setAttribute("href",`artist.html?id=${album.artist.id}`);
+  bandName.innerText = `${album.artist.name}`;
+
+  let anno = document.createElement("span");
+  let numTrack = document.createElement("span");
+  let durata = document.createElement("span");
+
+  anno.classList.add("fw-bold", "text-light","d-inline");
+  anno.innerText = `${album.release_date.slice(0,4)} • `; //Confermare come cosa se effettivamente fattibile con gli 0-4 come parametri
+  numTrack.classList.add("fw-bold", "text-light","d-inline");
+  numTrack.innerText = `${album.nb_tracks} brani • `;
+  durata.classList.add("opacity-75","d-inline");
+  durata.innerText = convertToMinSec(album.duration);
+
+  //albumInfo.innerHTML = `${anno.innerText} • ${numTrack.innerHTML} • ${durata.innerHTML}.`
+  albumInfo.append(anno,numTrack,durata);
+
+  //ANno  BOLD
+  //numeroBrani BOLD
+  //Durata Spento
+
+
+
 };
 
 
@@ -35,3 +84,9 @@ function toggleMenu() {
   const sidebar = document.getElementById('sidebar');
   sidebar.classList.toggle('expanded');
 };
+
+function convertToMinSec(seconds) {
+  const minutes = Math.floor(seconds / 60); // ottieni i minuti
+  const remainingSeconds = seconds % 60;  // ottieni i secondi rimanenti
+  return `${minutes}min ${remainingSeconds}sec`;
+}
