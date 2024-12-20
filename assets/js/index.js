f85035e01e2d1fcbff5004c63ab1dc65fca20017
Federico Tonti <148573975+Friedrich3@users.noreply.github.com>
2024-12-20 10:39:52 +0100

    fix dimensione libreria

--- assets/js/index.js
+++ assets/js/index.js
@@ -665,10 +665,8 @@ function printLibrary() {
   //console.log(preferiti)
   preferiti.forEach((element) => {
     let music = JSON.stringify(element);
-
     const popularBody = document.createElement("div");
     popularBody.className = "d-flex mb-2 hover-custom";
-    popularBody.setAttribute("onclick", `addMusic(${music})`);
     libraryList.appendChild(popularBody);
 
     //SECONDA SEZIONE: COVER ALBUM + TITOLO
@@ -677,6 +675,7 @@ function printLibrary() {
     popCover.setAttribute("src", element.albumCover);
     popCover.setAttribute("width", "25px");
     popCover.setAttribute("height", "25px");
+    popCover.setAttribute("onclick", `addMusic(${music})`);
     popularBody.appendChild(popCover);
 
     const info = document.createElement("div");
@@ -684,19 +683,19 @@ function printLibrary() {
     popularBody.appendChild(info);
 
     const a = document.createElement("p");
-    a.className = "m-0 ps-2 fs-small";
+    a.className = "m-0 ps-2 fs-small custom-text-library";
     a.innerText = element.artistName;
     info.appendChild(a);
 
     const b = document.createElement("p");
-    b.className = "m-0 ps-2 fs-small";
+    b.className = "m-0 ps-2 fs-small custom-text-library";
     b.innerText = element.trackTitle;
     info.appendChild(b);
 
     const trashBtn=document.createElement('button');
     trashBtn.className='btn ms-auto';
     trashBtn.innerHTML='<i class="bi bi-trash3 grey-icon fs-small"></i>';
-    trashBtn.setAttribute("onclick","");
+    trashBtn.setAttribute("onclick",`likeFeature(${music})`);
     popularBody.appendChild(trashBtn);
   });
 };
